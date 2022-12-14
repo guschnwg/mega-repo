5f3f80b9f91d156e155ba9281e2645d5015e6f91
Giovanna Zanardini <guschoneweg@hotmail.com>
2022-12-14 15:55:47 -0300

    docker datadog example

---
+++ docker-compose-example/web/app.py
@@ -0,0 +1,65 @@
+from flask import Flask
+from redis import Redis
+import os
+import threading
+
+# Add and initialize Datadog monitoring.
+from datadog import initialize, statsd
+initialize(statsd_host=os.environ.get('DATADOG_HOST'))
+
+
+from ddtrace import patch, tracer
+
+patch(logging=True)
+
+import logging
+
+
+FORMAT = ('%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] '
+          '[dd.service=%(dd.service)s dd.env=%(dd.env)s dd.version=%(dd.version)s dd.trace_id=%(dd.trace_id)s dd.span_id=%(dd.span_id)s] '
+          '- %(message)s')
+logging.basicConfig(format=FORMAT)
+log = logging.getLogger(__name__)
+log.level = logging.INFO
+
+
+app = Flask(__name__)
+redis = Redis(host='redis', port=6379)
+
+@tracer.wrap(name='my_resource1')
+@app.route('/')
+def hello():
+    # Increment the Datadog counter.
+    statsd.increment('docker_compose_example.page.views')
+
+    redis.incr('hits')
+
+    log.info('function1 has executed',
+        extra={
+            'job_category': 'test_function',
+            'logger.name': 'my_json',
+            'logger.thread_name' : threading.currentThread().name
+        }
+    )
+
+    return 'Hello World! I have been seen %s times.' % redis.get('hits')
+
+@app.route('/two')
+def two():
+    # Increment the Datadog counter.
+    statsd.increment('docker_compose_example.page.views')
+
+    redis.incr('hits')
+
+    log.info('function1 has executed',
+        extra={
+            'job_category': 'test_function',
+            'logger.name': 'my_json',
+            'logger.thread_name' : threading.currentThread().name
+        }
+    )
+
+    return 'Hello World! I have been seen %s times.' % redis.get('hits')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
