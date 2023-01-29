330b92dfb43a27de62d72ad5382d7e6b515c7f2b
Giovanna Zanardini <guschoneweg@hotmail.com>
2023-01-29 21:40:00 +0000

    More

--- tcc/src/App.js
+++ tcc/src/App.js
@@ -1,16 +1,20 @@
 import WORLD from "./world.json";
+import GAME from "./game.json";
 
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+
+
+import { GoogleMap, StreetViewPanorama, useJsApiLoader } from '@react-google-maps/api';
 
 import useMouse from '@react-hook/mouse-position';
 
-const Country = ({ country, selected, onClick, onMouseEnter, onMouseLeave }) => {
+const Country = ({ country, disabled, selected, onClick, onMouseEnter, onMouseLeave }) => {
   const ref = useRef(false);
 
   return (
     <path
-      className={`land ${selected && 'selected'} ${country.continent}`}
+      className={`land ${selected && 'selected'} ${disabled && 'disabled'} ${country.continent}`}
       {...country}
       onMouseEnter={() => onMouseEnter(country)}
       onMouseLeave={() => onMouseLeave(country)}
@@ -21,7 +25,7 @@ const Country = ({ country, selected, onClick, onMouseEnter, onMouseLeave }) => 
   );
 }
 
-const World = ({ selectedCountry, onClick, onMouseEnter, onMouseLeave }) => {
+const World = ({ disabledCountries, selectedCountry, onClick, onMouseEnter, onMouseLeave }) => {
   return (
     <svg
       id='map'
@@ -33,6 +37,7 @@ const World = ({ selectedCountry, onClick, onMouseEnter, onMouseLeave }) => {
         {WORLD.map(country => (
           <Country
             key={country.id}
+            disabled={disabledCountries.some(d => d.id === country.id)}
             selected={selectedCountry?.id === country.id}
             country={country}
             onClick={onClick}
@@ -62,16 +67,16 @@ const Tooltip = ({ country }) => {
   )
 }
 
-function Guess() {
+function Guess({ guesses, onGuess, onHide }) {
   const [selectedCountry, setSelectedCountry] = useState();
 
   return (
-    <div id='guess-container'>
+    <div className='guess-container'>
       <TransformWrapper
-        initialScale={4}
         centerOnInit
         doubleClick={{ disabled: true }}
         zoomAnimation={{ disabled: true }}
+        disablePadding
       >
         {(utils) => (
           <>
@@ -82,25 +87,207 @@ function Guess() {
 
             <TransformComponent>
               <World
+                disabledCountries={guesses.filter(g => !g.isRight).map(g => g.data.country)}
                 selectedCountry={selectedCountry}
                 onMouseEnter={setSelectedCountry}
                 onMouseLeave={() => setSelectedCountry(null)}
-                onClick={country => alert(country.title)}
+                onClick={country => onGuess({ time: Date.now(), country })}
               />
             </TransformComponent>
           </>
         )}
       </TransformWrapper>
       <Tooltip country={selectedCountry} />
+
+      <button className="guess-close" onClick={onHide}>X</button>
+    </div>
+  );
+}
+
+function StreetView({ country }) {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY || '',
+  });
+
+  if (!isLoaded) {
+    return null;
+  }
+
+  return (
+    <GoogleMap
+      mapContainerStyle={{ height: '100%', width: '100%' }}
+    >
+      <StreetViewPanorama
+        options={{
+          position: country.coordinates,
+          pov: { heading: 0, pitch: 0 },
+          zoom: 1,
+          visible: true,
+          enableCloseButton: false,
+        }}
+      />
+    </GoogleMap>
+  )
+}
+
+function EndGame({ game }) {
+  return (
+    <pre>
+      {JSON.stringify(game, null, 2)}
+    </pre>
+  );
+}
+
+function Timer({ start, run, limit = 5, onEnd }) {
+  const [current, setCurrent] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (!run) return;
+
+      const next = (Date.now() - start) / 1000;
+      if (next > (limit + 1)) {
+        onEnd();
+      } else {
+        setCurrent(next);
+      }
+    }, 250);
+    return () => {
+      clearInterval(interval);
+    }
+  }, [start, run, limit, onEnd]);
+
+  return <span>{Math.floor(current)}s</span>;
+}
+
+function RightAttempt({ onNext }) {
+  return (
+    <div className='guess-container'>
+      OIOI
+      <button onClick={onNext}>Próximo nível</button>
+    </div>
+  )
+}
+
+function TimeExceeded({ onNext }) {
+  return (
+    <div className='guess-container'>
+      FUCK
+      <button onClick={onNext}>Próximo nível</button>
+    </div>
+  )
+}
+
+function GuessExceeded({ onNext }) {
+  return (
+    <div className='guess-container'>
+      FUCK
+      <button onClick={onNext}>Próximo nível</button>
+    </div>
+  )
+}
+
+function Game({ timeLimit, guessLimit }) {
+  const [level, setLevel] = useState(0);
+  const [time, setTime] = useState(Date.now());
+  const [guesses, setGuesses] = useState([]);
+  const [game, setGame] = useState([]);
+
+  const [showGuessAttempt, setShowGuessAttempt] = useState(false);
+  const [showRightAttempt, setShowRightAttempt] = useState(false);
+  const [showTimeExceeded, setShowTimeExceeded] = useState(false);
+  const [showGuessExceeded, setShowGuessExceeded] = useState(false);
+
+  const country = GAME.countries[level];
+
+  const handleGuess = guess => {
+    const isRight = guess.country.id === country.country;
+    setGuesses(prev => [...prev, { data: guess, isRight }]);
+
+    if (isRight) {
+      setShowGuessAttempt(false);
+      setShowRightAttempt(true);
+    }
+
+    if (guesses.length + 1 >= guessLimit) {
+      setShowGuessExceeded(true);
+    }
+  }
+
+  const handleNext = () => {
+    setLevel(prev => prev + 1);
+    setGame(prev => [...prev, { start: time, country, guesses }]);
+    setGuesses([]);
+    setTime(Date.now());
+
+    // Hide modals
+    setShowGuessAttempt(false);
+    setShowRightAttempt(false);
+    setShowTimeExceeded(false);
+    setShowGuessExceeded(false);
+  };
+
+  if (!country) {
+    return <EndGame game={game} />;
+  }
+
+  const canGuess = !showGuessAttempt && !showRightAttempt && !showTimeExceeded && !showGuessExceeded;
+  const timeRunning = !showTimeExceeded && !showRightAttempt && !showGuessExceeded;
+
+  return (
+    <div className='app'>
+      <div>
+        <Timer
+          start={time}
+          run={timeRunning}
+          limit={timeLimit}
+          onEnd={() => {
+            setShowTimeExceeded(true);
+            setShowGuessAttempt(false);
+          }}
+        />
+      </div>
+
+      <div className="game-container">
+        <StreetView
+          key={level}
+          country={country}
+        />
+      </div>
+
+      {showGuessAttempt && (
+        <Guess
+          guesses={guesses}
+          onGuess={handleGuess}
+          onHide={() => setShowGuessAttempt(false)}
+        />
+      )}
+
+      {showRightAttempt && <RightAttempt onNext={handleNext} />}
+
+      {showTimeExceeded && <TimeExceeded onNext={handleNext} />}
+
+      {showGuessExceeded && <GuessExceeded onNext={handleNext} />}
+
+      {canGuess && (
+        <button
+          className="guess-button"
+          onClick={() => setShowGuessAttempt(true)}
+        >
+          Já sei!
+        </button>
+      )}
     </div>
   );
 }
 
 function App() {
   return (
-    <div>
-      <Guess />
-    </div>
+    <Game
+      timeLimit={15}
+      guessLimit={5}
+    />
   )
 }
 
