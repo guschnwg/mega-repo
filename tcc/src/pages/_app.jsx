f9488126647f97c208ce5b14e74c2e40fe0651a0
Giovanna Zanardini <guschoneweg@hotmail.com>
2023-05-05 11:35:02 -0300

    Shito

--- tcc/src/pages/_app.jsx
+++ tcc/src/pages/_app.jsx
@@ -10,7 +10,7 @@ export const metadata = {
   }
 }
 
-export default function Layout({ \Component, pageProps }) {
+export default function Layout({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
