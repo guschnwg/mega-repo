bb660285cc4653042936d4ec566f555f17f68cda
Giovanna Zanardini <guschoneweg@hotmail.com>
2023-07-05 09:47:27 -0300

    Load the script from the folder

--- vscode-extension-to-handle-changelists/src/hooks.ts
+++ vscode-extension-to-handle-changelists/src/hooks.ts
@@ -2,23 +2,8 @@ import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as childProcess from 'child_process';
 
-const SCRIPT: string = `#!/bin/sh
-
-FILES=$(git diff --name-only --cached)
-CHANGELIST=".gitchangelist\\n$([ -f .gitchangelist ] && cat .gitchangelist | grep "^[^#]")"
-NOT_ALLOWED_FILES=""
-
-while IFS= read -r FILE; do
-  if [[ "$CHANGELIST" == *"$FILE"* ]]; then
-    NOT_ALLOWED_FILES="$NOT_ALLOWED_FILES\\n$FILE"
-  fi
-done <<< "$FILES"
-
-if [[ "$NOT_ALLOWED_FILES" != "" ]]; then
-  echo "You have stuff from a changelist in the commit, remove it, then 🚢"
-  echo $NOT_ALLOWED_FILES
-  exit 1
-fi`;
+const currentExt = vscode.extensions.all.find(ext => ext.packageJSON.name === 'changelists');
+const SCRIPT: string = fs.readFileSync(`${currentExt?.extensionPath}/src/script.sh`).toString().trim();
 //
 function getRepoRoot(workspaceFolder: string) {
   return childProcess.execSync(`cd ${workspaceFolder} && git rev-parse --show-toplevel`).toString().trim();
