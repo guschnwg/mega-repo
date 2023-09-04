1acb29a4de0ba5b64fe24d57bbf579000a099cd8
Giovanna Zanardini <guschoneweg@hotmail.com>
2023-09-04 19:44:43 -0300

    Ship it

--- arduino/motorzinho/motorzinho.ino
+++ arduino/motorzinho/motorzinho.ino
@@ -1,28 +1,39 @@
-#define INT_1_1 25
-#define INT_2_1 26
-#define INT_1_2 33
-#define INT_2_2 32
+// ESP32 Wrover Kit (all versions)
+#include <ESP32MX1508.h>
+
+#define LEFT_1 25 // Green
+#define LEFT_2 26 // Blue
+#define RIGHT_1 33 // Brown
+#define RIGHT_2 32 // Gray
+
+MX1508 leftMotor (LEFT_2, LEFT_1, 0, 1);
+MX1508 rightMotor (RIGHT_1, RIGHT_2, 2, 3);
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(INT_1_1, OUTPUT);
-  pinMode(INT_2_1, OUTPUT);
-  pinMode(INT_1_2, OUTPUT);
-  pinMode(INT_2_2, OUTPUT);
+  Serial.begin(115200);
 }
 
 void loop() {
-  digitalWrite(INT_1_1, LOW);
-  digitalWrite(INT_2_1, HIGH);
-  digitalWrite(INT_1_2, HIGH);
-  digitalWrite(INT_2_2, LOW);
+  if(Serial.available()) {
+    int left = Serial.readStringUntil(':').toInt();
+    int right = Serial.readStringUntil(':').toInt();
+    int time = Serial.readStringUntil('\n').toInt();
 
-  delay(250);
-  
-  digitalWrite(INT_1_1, HIGH);
-  digitalWrite(INT_2_1, LOW);
-  digitalWrite(INT_1_2, LOW);
-  digitalWrite(INT_2_2, HIGH);
+    if (left > 0) {
+      leftMotor.motorGo(left);
+    } else {
+      leftMotor.motorRev(left);
+    }
 
-  delay(250);
+    if (right > 0) {
+      rightMotor.motorGo(right);
+    } else {
+      rightMotor.motorRev(right);
+    }
+
+    delay(time);
+  }
+
+  leftMotor.motorStop();
+  rightMotor.motorStop();
 }
