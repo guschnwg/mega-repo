1718799d61ea4dc7707a9b71d3f7c89c8b8d5464
Giovanna Zanardini <guschoneweg@hotmail.com>
2023-09-11 10:31:53 -0300

    Read some stuff and etc etc

--- arduino/motorzinho/motorzinho.ino
+++ arduino/motorzinho/motorzinho.ino
@@ -1,5 +1,6 @@
 // ESP32 Wrover Kit (all versions)
 #include <ESP32MX1508.h>
+#include "BluetoothSerial.h"
 
 #define LEFT_1 25 // Green
 #define LEFT_2 26 // Blue
@@ -9,29 +10,42 @@
 MX1508 leftMotor (LEFT_2, LEFT_1, 0, 1);
 MX1508 rightMotor (RIGHT_1, RIGHT_2, 2, 3);
 
+BluetoothSerial SerialBT;
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+  #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
 void setup() {
   Serial.begin(115200);
+  SerialBT.begin("ESP32");
+}
+
+void move(Stream &serial) {
+  int left = serial.readStringUntil(':').toInt();
+  int right = serial.readStringUntil(':').toInt();
+  int time = serial.readStringUntil('\n').toInt();
+
+  if (left > 0) {
+    leftMotor.motorGo(left);
+  } else {
+    leftMotor.motorRev(left);
+  }
+
+  if (right > 0) {
+    rightMotor.motorGo(right);
+  } else {
+    rightMotor.motorRev(right);
+  }
+
+  delay(time);
 }
 
 void loop() {
-  if(Serial.available()) {
-    int left = Serial.readStringUntil(':').toInt();
-    int right = Serial.readStringUntil(':').toInt();
-    int time = Serial.readStringUntil('\n').toInt();
-
-    if (left > 0) {
-      leftMotor.motorGo(left);
-    } else {
-      leftMotor.motorRev(left);
-    }
-
-    if (right > 0) {
-      rightMotor.motorGo(right);
-    } else {
-      rightMotor.motorRev(right);
-    }
-
-    delay(time);
+  if (Serial.available()) {
+    move(Serial);
+  } else if (SerialBT.available()) {
+    move(SerialBT);
   }
 
   leftMotor.motorStop();
