98092c67c2f14c9b4f69d668ef94edb32c0d96c3
Giovanna Zanardini <giovanna.zanardini@nextroll.com>
2024-05-23 19:44:51 -0300

    Update the nginx conf

--- something_something/stream/main.py
+++ something_something/stream/main.py
@@ -1,18 +1,18 @@
 import cv2
 from mjpeg_streamer import MjpegServer, Stream
 
-cap = cv2.VideoCapture(0)
-
-stream = Stream("my_camera", size=(1366, 768), quality=50, fps=60)
-
 server = MjpegServer("0.0.0.0", 8080)
+
+cap = cv2.VideoCapture(0)
+stream = Stream("my_camera", size=(1366, 768), quality=50, fps=60)
 server.add_stream(stream)
+
 server.start()
 
 while True:
     ret, frame = cap.read()
     if not ret:
-        break
+        continue
 
     stream.set_frame(frame)
 
