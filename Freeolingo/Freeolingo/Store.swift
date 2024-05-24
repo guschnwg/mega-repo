de666a73afa029773aab4f791b040418cc7e754f
Giovanna Zanardini <giznrdni@gmail.com>
2024-05-24 16:43:48 -0300

    Add new methods for retrieving available courses and specific courses

--- Freeolingo/Freeolingo/Store.swift
+++ Freeolingo/Freeolingo/Store.swift
@@ -28,6 +28,8 @@ struct SessionAttempt : Codable, Identifiable {
 class Store: ObservableObject {
     @Published var sessionAttempts: [SessionAttempt] = []
     @Published var sessionAttemptMap: [String: SessionAttempt] = [:]
+    @Published var availableCourses: [AvailableCourse] = []
+    @Published var courses: [Course] = []
     
     private static func fileURL() throws -> URL {
         try FileManager.default.url(for: .documentDirectory,
@@ -51,6 +53,60 @@ class Store: ObservableObject {
             fatalError(error.localizedDescription)
         }
         
+    }
+    
+    func getAvailableCourses(fromLanguages: [String]) {
+        // TODO: Keep this persistent
+
+        if !self.availableCourses.isEmpty {
+            return
+        }
+
+        let baseURL = "http://localhost:8080"
+        let token = UserDefaults.standard.value(forKey: "duolingo_token")!
+        let path = "getCourseList/\(token)/"
+        
+        let url = URL(string: "\(baseURL)/\(path)")!
+        let task = URLSession.shared.dataTask(with: url) { data, _, _ in
+            do {
+                let courses = try JSONDecoder().decode([AvailableCourse].self, from: data!)
+                DispatchQueue.main.async {
+                    self.availableCourses = courses.filter { fromLanguages.contains($0.fromLanguage) }
+                }
+            } catch {
+                print(error)
+            }
+        }
+        task.resume()
+    }
+    
+    func getCourse(languageSettings: LanguageSettings) {
+        // TODO: Keep this persistent
+
+        if self.courses.contains(where: { $0.fromLanguage == languageSettings.fromLanguage && $0.learningLanguage == languageSettings.learningLanguage }) {
+            return
+        }
+        
+        let baseURL = "http://localhost:8080"
+        let token = UserDefaults.standard.value(forKey: "duolingo_token")!
+        let path = "getSpecificCourse/\(token)/\(languageSettings.fromLanguage)/\(languageSettings.learningLanguage)"
+        
+        let url = URL(string: "\(baseURL)/\(path)")!
+        let task = URLSession.shared.dataTask(with: url) { data, _, _ in
+            do {
+                let course = try JSONDecoder().decode(Course.self, from: data!)
+                DispatchQueue.main.async {
+                    self.courses.append(course)
+                }
+            } catch {
+                print(error)
+            }
+        }
+        task.resume()
+    }
+    
+    func viewSession(course: Course, section: Section, unit: Unit, level: Level, session: Session) {
+        print("View session \(session)")
     }
     
     func save() {
