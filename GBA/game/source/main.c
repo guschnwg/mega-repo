715d971325a88b1d1d788c71409a0b8ff583044f
Giovanna Zanardini <giznrdni@gmail.com>
2024-12-23 15:54:18 -0300

    The game

---
+++ GBA/game/source/main.c
@@ -0,0 +1,15 @@
+// make run
+
+#include <tonc.h>
+
+int main() {
+  REG_DISPCNT = DCNT_MODE3 | DCNT_BG2;
+  tte_init_bmp_default(3);
+
+  while (1) {
+    vid_vsync();
+    m3_fill(CLR_BLACK);
+    tte_write("#{P:90,80}Hello World!");
+  }
+  return 0;
+}
