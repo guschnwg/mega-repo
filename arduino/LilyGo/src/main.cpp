e4ee6bc50ac49a2a50a8dc449c27568b778fa5ee
Giovanna Zanardini <giznrdni@gmail.com>
2025-03-27 16:16:58 -0300

    Fix it again

--- arduino/LilyGo/src/main.cpp
+++ arduino/LilyGo/src/main.cpp
@@ -55,7 +55,7 @@ int vref = 1100; // Don't know why it's 1100
 void setup_battery_voltage() {
   esp_adc_cal_characteristics_t adc_chars;
   esp_adc_cal_value_t val_type = esp_adc_cal_characterize(
-      ADC_UNIT_2, ADC_ATTEN_DB_12, ADC_WIDTH_BIT_12, 1100, &adc_chars);
+      ADC_UNIT_2, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 1100, &adc_chars);
 
   if (val_type == ESP_ADC_CAL_VAL_EFUSE_VREF) {
     Serial.printf("eFuse Vref: %umV\r\n", adc_chars.vref);
