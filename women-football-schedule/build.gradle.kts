3dd173fd4d4de75cbac2353d9a3cab264ebf8360
Giovanna Alpendre <giovanna@GIOVANNA-ZANARDINI.local>
2025-04-02 18:14:17 -0300

    Add WFS

---
+++ women-football-schedule/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    application
+    kotlin("jvm") version "2.1.20"
+}
+
+group = "org.giovanna"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.seleniumhq.selenium:selenium-java:4.30.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+}
+
+application {
+    mainClass.set("com.giovanna.stuff.HelloKt")
+}
+
+tasks {
+    test {
+        testLogging {
+            events("failed", "passed", "started")
+        }
+    }
+    val fatJar = register<Jar>("fatJar") {
+        dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources"))
+        archiveClassifier.set("standalone")
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+        manifest { attributes(mapOf("Main-Class" to application.mainClass)) }
+        val sourcesMain = sourceSets.main.get()
+        val contents = configurations.runtimeClasspath.get()
+            .map { if (it.isDirectory) it else zipTree(it) } +
+                sourcesMain.output
+        from(contents)
+    }
+    build {
+        dependsOn(fatJar)
+    }
+}
