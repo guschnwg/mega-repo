00991c0af15974148e4c04141e2c3f99bb831713
Giovanna Zanardini <giznrdni@gmail.com>
2025-07-05 17:36:21 -0300

    add R stuff

---
+++ R/ProjetoFinal/_template_aula.R
@@ -0,0 +1,3 @@
+project_root_path <- paste0(getwd(), "/ProjetoFinal")
+source(paste0(project_root_path, "/_definitions.R"))
+source(paste0(project_root_path, "/_install_load_packages.R"), encoding = encoding)
