d665e7e3becd27d5767906d70ef09b7b4c8eb835
Giovanna <giovanna.zanardini@nextroll.com>
2025-10-07 19:23:10 -0300

    IDK if this is an improvement

--- app_framework/api/src/handler.py
+++ app_framework/api/src/handler.py
@@ -8,6 +8,12 @@ import mimetypes
 from utils import hash_password, verify_password
 from db import Db
 
+class HandlerException(Exception):
+    def __init__(self, code, data):
+        self.code = code
+        self.data = data
+        super().__init__()
+
 class BaseHandler(BaseHTTPRequestHandler):
     db: Db
 
@@ -21,20 +27,20 @@ class BaseHandler(BaseHTTPRequestHandler):
             try:
                 return json.loads(raw_input)
             except json.JSONDecodeError:
-                return {}
+                raise HandlerException(400, {"error": "Invalid JSON"})
 
         if self.headers.get('Content-Type') == 'application/x-www-form-urlencoded':
             try:
                 return dict(parse_qsl(raw_input))
             except ValueError:
-                return {}
+                raise HandlerException(400, {"error": "Invalid URL-encoded data"})
 
-        return {}
+        return None
 
     def _set_response(
         self,
-        code=200,
-        content_type='text/html',
+        code: int = 200,
+        content_type: str = 'text/html',
         data: str | dict | bytes = "",
     ):
         self.send_response(code)
@@ -44,21 +50,12 @@ class BaseHandler(BaseHTTPRequestHandler):
            return
 
         if isinstance(data, dict) or isinstance(data, list) :
-            return self.wfile.write(json.dumps(data).encode('utf-8'))
+            self.wfile.write(json.dumps(data).encode('utf-8'))
+            return
 
         self.wfile.write(data.encode('utf-8') if isinstance(data, str) else data)
 
     #
-
-    def _login(self, data):
-        possible_user = self.db.get_user_by_email_with_password(data["email"])
-        if not possible_user or not possible_user["active"]:
-            return None
-
-        if not verify_password(data["password"], possible_user["password"]):
-            return None
-
-        return self.db.create_session(possible_user["id"], secrets.token_urlsafe(16), datetime.now() + timedelta(hours=1))
 
     def _get_token(self, cookie):
         if not cookie or "session=" not in cookie:
@@ -73,60 +70,99 @@ class BaseHandler(BaseHTTPRequestHandler):
     def _get_user_from_token(self, cookie):
         token = self._get_token(cookie)
         if not token:
-            return None
+            raise HandlerException(401, {"error": "Invalid token"})
 
         session = self.db.get_session_by_token(token)
         if not session:
-            return None
+            raise HandlerException(401, {"error": "Invalid token"})
 
         if session["valid_until"] < datetime.now():
-            return None
+            raise HandlerException(401, {"error": "Session expired"})
 
         user = self.db.get_user(session["user_id"])
         if not user:
-            return None
+            raise HandlerException(404, {"error": "User not found"})
 
         if not user["active"]:
-            return None
+            raise HandlerException(401, {"error": "Inactive"})
 
         return user
 
     def _delete_session(self, cookie):
-        token = self._get_token(cookie)
-        if not token:
-            return
+        if token := self._get_token(cookie):
+            self.db.delete_session_by_token(token)
 
-        self.db.delete_session_by_token(token)
-    #
-
-    def GET_me(self, user, data):
-        return 200, user
-
-    def GET_list_users(self, user, data):
-        if "admin" not in user["roles"]:
-            return 403, {"error": "Unauthorized"}
-
-        return 200, self.db.get_users()
+    def _json(self, code, data):
+        return self._set_response(
+            code=code,
+            content_type='application/json',
+            data=data,
+        )
 
     #
 
-    def POST_create_user(self, user, data):
-        if "admin" not in user["roles"]:
-            return 403, {"error": "Unauthorized"}
+    def GET__logout(self):
+        self._delete_session(self.headers.get('Cookie'))
+        self.send_response(302)
+        self.send_header("Location", "/")
+        return self.end_headers()
 
+    def GET__api__me(self):
+        user = self._get_user_from_token(self.headers.get('Cookie'))
+        return self._json(200, user)
+
+    def GET__api__list_users(self):
+        user = self._get_user_from_token(self.headers.get('Cookie'))
+        if "admin" not in user["roles"]:
+            raise HandlerException(403, {"error": "Unauthorized"})
+
+        return self._json(200, self.db.get_users())
+
+    #
+
+    def POST__login(self):
+        data = self._get_input_data()
+        if data:
+            possible_user = self.db.get_user_by_email_with_password(data["email"])
+            if not possible_user or not possible_user["active"]:
+                return None
+
+            if not verify_password(data["password"], possible_user["password"]):
+                return None
+
+            session = self.db.create_session(possible_user["id"], secrets.token_urlsafe(16), datetime.now() + timedelta(hours=1))
+            if not session:
+                self.send_response(302)
+                self.send_header("Location", "/login")
+                return self.end_headers()
+
+        self.send_response(302)
+        self.send_header('Set-Cookie', f"session={session['token']}; HttpOnly; Secure; SameSite=Strict; Path=/")
+        self.send_header("Location", "/")
+        return self.end_headers()
+
+    def POST__api__create_user(self):
+        user = self._get_user_from_token(self.headers.get('Cookie'))
+        if "admin" not in user["roles"]:
+            raise HandlerException(403, {"error": "Unauthorized"})
+
+        data = self._get_input_data()
         existing_user = self.db.get_user_by_email(data["email"])
         if existing_user:
-            return 409, {"error": "Email already exists"}
+            raise HandlerException(409, {"error": "Email already exists"})
 
-        return 201, self.db.create_user(data["email"], hash_password(data["password"]), data.get("roles") or ["user"], True)
+        new_user = self.db.create_user(data["email"], hash_password(data["password"]), data.get("roles") or ["user"], True)
+        return self._json(201, new_user)
 
-    def POST_update_user(self, user, data):
+    def POST__api__update_user(self):
+        user = self._get_user_from_token(self.headers.get('Cookie'))
         if "admin" not in user["roles"]:
-            return 403, {"error": "Unauthorized"}
+            raise HandlerException(403, {"error": "Unauthorized"})
 
+        data = self._get_input_data()
         user_to_update = self.db.get_user_with_password(data["id"])
         if not user_to_update:
-            return 404, {"error": "User not found"}
+            raise HandlerException(404, {"error": "User not found"})
 
         updated_user = self.db.update_user(
             data["id"],
@@ -136,47 +172,21 @@ class BaseHandler(BaseHTTPRequestHandler):
             data["active"] if "active" in data else user_to_update["active"]
         )
 
-        return 200, updated_user
+        return self._json(200, updated_user)
 
     #
 
     def do_GET(self):
-        if self.path == "/logout":
-            self._delete_session(self.headers.get('Cookie'))
-            self.send_response(302)
-            self.send_header("Location", "/")
-            return self.end_headers()
-
-        if self.path.startswith("/api"):
-            user = self._get_user_from_token(self.headers.get('Cookie'))
-            if not user:
-                return self._set_response(
-                    code=401,
-                    content_type='application/json',
-                    data={"error": "Invalid token"},
-                )
-
-            endpoint = self.path.replace("/api/", "")
-            if hasattr(self, f"GET_{endpoint}"):
-                try:
-                    code, data = getattr(self, f"GET_{endpoint}")(user, {})
-                    return self._set_response(
-                        code=code,
-                        content_type='application/json',
-                        data=data,
-                    )
-                except Exception as e:
-                    print(f"Unexpected error: {e}")
-                    return self._set_response(
-                        code=500,
-                        content_type='application/json',
-                        data={"error": "Unexpected error"},
-                    )
-
-            return self._set_response(
-                content_type='application/json',
-                data={"error": "Endpoint not found"},
-            )
+        method_name = self.path.replace("/", "__")
+        method_name = f"GET{method_name}"
+        if hasattr(self, method_name):
+            try:
+                return getattr(self, method_name)()
+            except HandlerException as e:
+                return self._json(e.code, e.data)
+            except Exception as e:
+                print(f"Unexpected error: {e}")
+                return self._json(500, {"error": "Unexpected error"})
 
         if self.path.startswith("/dist"):
             with open(f'/app{self.path}', 'rb') as file:
@@ -188,46 +198,19 @@ class BaseHandler(BaseHTTPRequestHandler):
             return self._set_response(data=file.read())
 
     def do_POST(self):
-        if self.path == "/login":
-            session = self._login(self._get_input_data())
-            if not session:
-                self.send_response(302)
-                self.send_header("Location", "/login")
-                return self.end_headers()
+        method_name = self.path.replace("/", "__")
+        method_name = f"POST{method_name}"
+        if hasattr(self, method_name):
+            try:
+                return getattr(self, method_name)()
+            except HandlerException as e:
+                return self._json(e.code, e.data)
+            except Exception as e:
+                print(f"Unexpected error: {e}")
+                return self._json(500, {"error": "Unexpected error"})
 
-            self.send_response(302)
-            self.send_header('Set-Cookie', f"session={session['token']}; HttpOnly; Secure; SameSite=Strict; Path=/")
-            self.send_header("Location", "/")
-            return self.end_headers()
-
-        if self.path.startswith("/api/"):
-            user = self._get_user_from_token(self.headers.get('Cookie'))
-            if not user:
-                return self._set_response(
-                    code=401,
-                    content_type='application/json',
-                    data={"error": "Invalid token"},
-                )
-
-            endpoint = self.path.replace("/api/", "")
-            if hasattr(self, f"POST_{endpoint}"):
-                try:
-                    code, data = getattr(self, f"POST_{endpoint}")(user, self._get_input_data())
-                    return self._set_response(
-                        code=code,
-                        content_type='application/json',
-                        data=data,
-                    )
-                except Exception as e:
-                    print(f"Unexpected error: {e}")
-                    return self._set_response(
-                        code=500,
-                        content_type='application/json',
-                        data={"error": "Unexpected error"},
-                    )
-
-            return self._set_response(
-                content_type='application/json',
-                data={"echo": self._get_data()},
-            )
+        return self._set_response(
+            content_type='application/json',
+            data={"echo": self._get_data()},
+        )
 
